8fac0636a5275c9c4df4c875b9ad975ad402ba51
gaurangvyas98 <gvyas1234@gmail.com>
2021-06-05 22:09:38 +0530

    boilerplate for graphQL

---
+++ server.js
@@ -0,0 +1,33 @@
+const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose')
+const resolvers = require('./graphQl/resolvers')
+const typeDefs = require('./graphQl/typeDef')
+const { MONGO_URI } = require('./config/env.json')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const connectMongoDB = async() => {
+    try{
+        const conn = await mongoose.connect(MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+
+        console.log(`Connected to MongoDB ${conn.connection.host}`)
+    }
+    catch(error){
+        console.log(`Error connnecting to MongoDB ${error.message}`)
+        process.exit(1)
+    }
+}
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+
+  connectMongoDB();
+});
